87d7ac75a2bbdba992757b4030ddd4b794461b4f
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-02 22:20:04 +0500

    Added is_palindrome function's implementation using two pointers pattern

--- LEET_CODE_DSA_PROBLEMS/is_palindrome.cpp
+++ LEET_CODE_DSA_PROBLEMS/is_palindrome.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool is_palindrome(string random_words)
 {
     int left = 0;
-    int right = sizeof(random_words) / sizeof(random_words[0]) - 1; // This will assign the last index to variable 'right'
+    int right = random_words.length() - 1; // This will assign the last index to variable 'right'
 
     while (left < right)
     {
