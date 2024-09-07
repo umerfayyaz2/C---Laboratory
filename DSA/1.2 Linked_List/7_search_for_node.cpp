a47476c2ef2b1ed3fcb93e6b7f46bddf0b855737
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-07 21:05:36 +0500

    added is list empty function.

--- DSA/1.2 Linked_List/7_search_for_node.cpp
+++ DSA/1.2 Linked_List/7_search_for_node.cpp
@@ -10,9 +10,12 @@ int search_for_node(NodeType *&head, int target)
 {
     int position = 1;         // Works as index in Linked_list
     NodeType *current = head; // indicates the current position of node
+    bool is_list_empty = true;
 
     while (current != nullptr)
     {
+        is_list_empty = false;
+
         if (current->value == target)
         {
             return position;
@@ -23,5 +26,11 @@ int search_for_node(NodeType *&head, int target)
             current = current->next;
         }
     }
+
+    if (is_list_empty)
+    {
+        return -1;
+    }
+
     return -1; // if the linked list is empty.
 }
