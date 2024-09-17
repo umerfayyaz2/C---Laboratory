eb5e1c2d6da454ca437cfb8bb66dc5ae2861cf49
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-17 16:17:45 +0500

    Changes in vector

--- OOP/Vectors/0_intro_to_vectors.cpp
+++ OOP/Vectors/0_intro_to_vectors.cpp
@@ -7,3 +7,9 @@ vector<int> myVector; // Example of declaring a vector of integers
 // This is how a vector's value can be accessed.
 vector<int> vec = {1, 2, 3};
 int x = vec[2]; // Accessing element at index 2, x is 3
+
+vector<int> v = {1, 2, 3};
+cout << v.size(); // Output: 3
+
+vector<int> v(5);     // Initializes vector with capacity for 5 elements
+cout << v.capacity(); // Output: 5
