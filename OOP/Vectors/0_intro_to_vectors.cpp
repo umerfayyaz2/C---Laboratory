af4e77681224c4b9711c91fc545b8241fb8b92f1
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-17 16:21:32 +0500

    reserve function addition.

--- OOP/Vectors/0_intro_to_vectors.cpp
+++ OOP/Vectors/0_intro_to_vectors.cpp
@@ -8,8 +8,14 @@ vector<int> myVector; // Example of declaring a vector of integers
 vector<int> vec = {1, 2, 3};
 int x = vec[2]; // Accessing element at index 2, x is 3
 
+// size function shows the total num of elems in a vector
 vector<int> v = {1, 2, 3};
 cout << v.size(); // Output: 3
 
+// Capacity: The total number of elements a vector can hold without reallocating memory.
 vector<int> v(5);     // Initializes vector with capacity for 5 elements
 cout << v.capacity(); // Output: 5
+
+// reserve function to resorve memory for future use only if you know how much memory you have to occupy
+vector<int> v;
+v.reserve(10); // Reserves memory for 10 elements
