d26c2c776e43808bf61a40a9938410aefb1d2e74
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-09-17 15:09:56 +0500

    added vector implementation details.

---
+++ OOP/Vectors/0_intro_to_vectors.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> myVector; // Example of declaring a vector of integers
+
+// This is how a vector's value can be accessed.
+vector<int> vec = {1, 2, 3};
+int x = vec[2]; // Accessing element at index 2, x is 3
