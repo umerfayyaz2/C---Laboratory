b736adcdac8ec2eef4f757ebc7d5d170eaaad54e
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-12-21 17:34:35 +0500

    Added bubble sort algo solution

---
+++ DSA_Exam_Preparations/Heap/bubble_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// Solution for bubble sort algorithm
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array in ascending order: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
